c5573d9aa7667e040461f52759788bba4be2d88b
Patrick Ayiko Onia <60845728+Paikonia@users.noreply.github.com>
2024-11-21 20:00:30 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+//Render blogs and create a form to get create the blogs.
+
+function App() {
+  return (
+    <div>
+      <h1>Blogs site</h1>
+    </div>
+  );
+}
+
+export default App;
